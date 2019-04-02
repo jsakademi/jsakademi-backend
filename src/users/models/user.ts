934d37e0adc95a533e16133a25f7e8bc11db3ebd
Poyraz <poyrazylmaz@gmail.com>
2019-04-02 20:35:18 +0300

    update user model with new fields and descriptions...

--- src/users/models/user.ts
+++ src/users/models/user.ts
@@ -1,10 +1,28 @@
 import { ObjectType, Field, ID } from 'type-graphql';
 
-@ObjectType()
+@ObjectType({ description: 'The User Model' })
 export class User {
   @Field(type => ID)
   id: string;
 
-  @Field()
-  name: string;
+  @Field({ description: 'Firstname of the user' })
+  firstname: string;
+
+  @Field({ description: 'Lastname of the user' })
+  lastname: string;
+
+  @Field({ description: 'Email of the user' })
+  email: string;
+
+  @Field({
+    nullable: true,
+    description: 'Biography of the user',
+  })
+  bio?: string;
+
+  @Field({
+    nullable: true,
+    description: 'Avatar url of the user',
+  })
+  avatar?: string;
 }
