867938ff5608c9d33c0c0d10f046de439c5dc24c
POYRAZ YILMAZ <poyrazylmaz@gmail.com>
2019-04-05 09:08:47 +0300

    tags module created with basic resolver and service and added to application module...

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { UsersModule } from './users/users.module';
       autoSchemaFile: 'schema.graphql',
     }),
     UsersModule,
+    TagsModule,
   ],
 })
 export class AppModule {}
